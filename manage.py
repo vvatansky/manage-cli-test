8467adc8e1d414c672038c62e0b0374f6bf30574
vvatansky <vladimir.vatansky@yandex.ru>
2026-01-21 16:59:19 +0300

    init commit

---
+++ manage.py
@@ -0,0 +1,45 @@
+from sys import argv
+
+
+class Help:
+    def run(self, args):
+        print(f'-- Run task <help> with arguments: {args}')
+
+
+class Manager:
+    def __init__(self):
+        self.tasks = {
+            'help': Help()
+        }
+
+    def parse_args(self, *args):
+
+        result = {'list': []}
+        is_wait, anchor = False, None
+        for x in args:
+            if is_wait:
+                result[anchor] = x
+                is_wait = False
+            else:
+                if x[0] == '-':
+                    anchor = x
+                    is_wait = True
+                else:
+                    result['list'].append(x)
+        return result
+
+
+    def run(self, task, *args):
+        obj = self.tasks.get(task, None)
+        if obj:
+            obj.run(self.parse_args(*args))
+
+
+def main():
+    if len(argv) < 2: exit()
+    _, task, *args = argv
+    Manager().run(task, *args)
+
+
+if __name__ == '__main__':
+    main()
